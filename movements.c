aa088ccd49abc8596405eda7381f43f3e0630ffb
Natália Medeiros Da Silva De Almeida <natalia@Natalias-MBP.home>
2024-04-17 21:05:44 +0200

    update

--- movements.c
+++ movements.c
@@ -6,7 +6,7 @@
 /*   By: natalia <natalia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 14:01:35 by natalia           #+#    #+#             */
-/*   Updated: 2024/04/17 20:46:48 by natalia          ###   ########.fr       */
+/*   Updated: 2024/04/17 21:05:20 by natalia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,41 @@ void	print_moves(t_game *game)
 {
 	char	*temp;
 	char	print_moves[10];
+	char	print_collectables[20];
 
 	temp = ft_itoa(game->total_moves);
 	strcpy(print_moves, "Moves: ");
 	strcat(print_moves, temp);
+	free(temp);
+	temp = ft_itoa(game->collected_collectables);
+	strcpy(print_collectables, "Collected: ");
+	strcat(print_collectables, temp);
+	free(temp);
+	strcat(print_collectables, "/");
+	temp = ft_itoa(game->total_collectable);
+	strcat(print_collectables, temp);
 	mlx_delete_image(game->mlx, game->images->moves_print);
+	mlx_delete_image(game->mlx, game->images->collected_print);
 	game->images->moves_print = mlx_put_string(game->mlx, print_moves,
 			16, game->height * 64 - 350);
+	game->images->collected_print = mlx_put_string(game->mlx, print_collectables,
+		16, game->height * 64 - 370);
 	free(temp);
 }
+
+// void	print_moves(t_game *game)
+// {
+// 	char	*temp;
+// 	char	print_moves[20];
+
+// 	temp = ft_itoa(game->total_moves);
+// 	strcpy(print_moves, "Moves: ");
+// 	strcat(print_moves, temp);
+// 	free(temp);
+// 	mlx_delete_image(game->mlx, game->images->moves_print);
+// 	game->images->moves_print = mlx_put_string(game->mlx, print_moves,
+// 			16, game->height * 64 - 350);
+// }
 
 void	print_collectables(t_game	*game)
 {
